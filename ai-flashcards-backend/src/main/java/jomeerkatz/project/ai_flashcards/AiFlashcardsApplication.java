2b84e30cb291984ab7e20791db433a35da5530fd
jomeerkatz <jokirillmeerkatz@outlook.de>
2025-12-11 23:07:57 +0100

    initial commit

---
+++ ai-flashcards-backend/src/main/java/jomeerkatz/project/ai_flashcards/AiFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package jomeerkatz.project.ai_flashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFlashcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFlashcardsApplication.class, args);
+	}
+
+}
